81f022fca3d1f921ecf38b60972421f8a1bbe754
Ian Nesbitt <ian.nesbitt@gmail.com>
2022-12-13 11:31:12 -0800

    adding main script and simple click commands

--- mnonboard/mnonboard.py
+++ mnonboard/mnonboard.py
@@ -5,4 +5,22 @@ import click
 
 from . import mnutils
 
+@click.command()
+@click.option("--jsonfile", default=None,
+              help="JSON file to load (defaults to None).")
+@click.option("--createjson", default=None,
+              help="Create a default blanked JSON file for the user to fill in themselves.")
+def main(loc):
+    """
+    Wrapper around opersist that simplifies the process of onboarding a new
+    member node to DataONE.
+    """
 
+    if loc is None:
+        # do the full user-driven info gathering process
+    else:
+        # grab the info from a json
+
+
+if __name__ == '__main__':
+    main()
