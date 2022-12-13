f5853ab7af84d70ea812a86ca145a59281aecef0
Ian Nesbitt <ian.nesbitt@gmail.com>
2022-12-13 08:32:31 -0800

    adding paths defs and logic

--- mnonboard/__init__.py
+++ mnonboard/__init__.py
@@ -9,3 +9,17 @@ logging.basicConfig(
     datefmt=LOG_DATE_FORMAT,
 )
 L = logging.getLogger("main")
+
+# absolute path of current file
+CUR_PATH_ABS = os.path.dirname(os.path.abspath(__file__))
+
+# relative path to nodes directory
+NODE_PATH_REL = 'instance/nodes/'
+
+def node_path(nodepath=NODE_PATH_REL, curpath=CUR_PATH_ABS):
+    """
+    Get the absolute path of the nodes directory where new members will go.
+    Currently the nodes directory lives at `../instance/nodes/` (relative to
+    the mnonboard dir that this file is in).
+    """
+    return os.path.abspath(os.path.join(CUR_PATH_ABS, '../', NODE_PATH_REL))
