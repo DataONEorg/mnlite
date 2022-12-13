98edb343e70f29f64ab37aed77cbeed00a2e0dd9
Ian Nesbitt <ian.nesbitt@gmail.com>
2022-12-12 16:44:37 -0800

    add logging

--- mnonboard/__init__.py
+++ mnonboard/__init__.py
@@ -3,3 +3,9 @@ import logging
 
 from opersist.cli import LOG_LEVELS, LOG_DATE_FORMAT, LOG_FORMAT
 
+logging.basicConfig(
+    level=LOG_LEVELS.get("INFO", logging.INFO),
+    format=LOG_FORMAT,
+    datefmt=LOG_DATE_FORMAT,
+)
+L = logging.getLogger("main")
