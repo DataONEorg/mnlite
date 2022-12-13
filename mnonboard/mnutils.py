a34a19af4af51f797783a1414ed5c51633c525d0
Ian Nesbitt <ian.nesbitt@gmail.com>
2022-12-12 16:45:08 -0800

    add the utils to use in data collection

--- mnonboard/mnutils.py
+++ mnonboard/mnutils.py
@@ -1,40 +1,38 @@
 import json
 import pyshacl
 
+from .defs import DEFAULT_JSON, FIELDS, FILL_FIELDS, SITEMAP_URLS
+from . import L
+
 def default_json():
-	"""
-	A function that spits out a json file to be used in onboarding.
-	"""
-    jstr = r'''{
-  "node": {
-    "node_id": "",
-    "state": "up",
-    "name": "",
-    "description": "",
-    "base_url": "",
-    "schedule": {
-      "hour": "*",
-      "day": "*",
-      "min": "0,10,20,30,40,50",
-      "mon": "*",
-      "sec": "0",
-      "wday": "?",
-      "year": "*"
-    },
-    "subject": "",
-    "contact_subject": ""
-  },
-  "data_folder": "data",
-  "content_database": "sqlite:///content.db",
-  "log_database": "sqlite:///eventlog.db",
-  "created": "",
-  "default_submitter": "",
-  "default_owner": "",
-  "spider": {
-    "sitemap_urls":[
-      ""
-    ]
-  }
-}
-'''
-    return json.loads(jstr)
+    """
+    A function that spits out a json file to be used in onboarding.
+    """
+    return json.loads(DEFAULT_JSON)
+
+def user_input():
+    """
+    We need a few pieces of information to fill the json fields.
+    """
+    for f in FIELDS:
+        if f in 'num_sitemap_urls':
+            while True:
+                FIELDS[f][1] = int(input(FIELDS[f][0]))
+                if FIELDS[f][1] > 1:
+                    break
+                else:
+                    L.warning("The number of database sitemap URLs can't be less than 1.")
+        else:
+            FIELDS[f][1] = input(FIELDS[f][0])
+    return FIELDS
+
+def sitemap_urls(num_urls):
+    """
+    Collect the sitemap URLs.
+    Usually there will be just one of these but we will prepare for more.
+    """
+    i = 0
+    while i < num_urls:
+        SITEMAP_URLS[i] = input("Sitemap URL #%s: " % (i+1))
+        i += 1
+    return SITEMAP_URLS
