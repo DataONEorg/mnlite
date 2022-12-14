0071d8e33a7b391906ab4dc28fcb28a2ac15a906
Ian Nesbitt <ian.nesbitt@gmail.com>
2022-12-14 10:19:12 -0800

    cleaning up mnonboard

--- mnonboard/mnonboard.py
+++ mnonboard/mnonboard.py
@@ -2,31 +2,29 @@ import os
 import sys
 import json
 import click
+import pyshacl
 
+from mnonboard import L
 import mnutils
 from opersist.cli import getOpersistInstance
 
 @click.command()
 @click.option("-j", "--jsonfile", default=None,
               help="JSON file to load (defaults to None).")
-@click.option("-c", "--createjson", default='node.json',
-              help="Create a default blanked JSON file for the user to fill in themselves.")
-def main(ctx, loc):
+@click.pass_context
+def main(ctx, jsonfile):
     """
     Wrapper around opersist that simplifies the process of onboarding a new
     member node to DataONE.
     """
 
-    if loc is None:
+    if jsonfile is None:
         # do the full user-driven info gathering process
         fields = mnutils.user_input()
-        fields['sitemap_urls'] = ['Sitemap URLs: ', '']
-        # get the sitemap urls as a list
-        fields['sitemap_urls'][1] = mnutils.sitemap_urls(fields['num_sitemap_urls'][1]) # type: ignore
     else:
         # grab the info from a json
-        pass
-
+        fields = mnutils.load_json(jsonfile)
+        mnutils.input_test(fields)
 
 if __name__ == '__main__':
     main()
