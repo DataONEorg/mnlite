2e8be6835bc5cb2b12c22ffe14f746087456ca87
Ian Nesbitt <ian.nesbitt@gmail.com>
2022-12-15 10:26:14 -0800

    adding logic for save and initialization

--- mnonboard/utils.py
+++ mnonboard/utils.py
@@ -1,5 +1,5 @@
 import json
-import pyshacl
+import subprocess
 
 from defs import DEFAULT_JSON
 from mnonboard import L
@@ -18,7 +18,9 @@ def load_json(loc):
     L.info('Loading member node json from %s' % loc)
     try:
         with open(loc, 'r') as f:
-            return json.load(f)
+            json.load(f)
+            L.info('File loaded from %s' % loc)
+            return
     except FileNotFoundError as e:
         L.error('File does not exist - %s' % e)
         exit(1)
@@ -31,6 +33,24 @@ def save_json(loc, jf):
     Output json to file.
     """
     L.info('Writing member node json to %s' % loc)
-    with open(loc, 'w') as f:
-        json.dump(jf, f)
+    try:
+        with open(loc, 'w') as f:
+            json.dump(jf, f)
+            L.info('File written to %s' % loc)
+            return
+    except FileExistsError as e:
+        L.error('File exists - %s' % e)
+        exit(1)
+    except Exception as e:
+        L.error('Error: %s' % e)
+        exit(1)
 
+def init_repo(loc):
+    '''
+    Initialize a new instance using opersist.
+    '''
+    try:
+        assert subprocess.call('opersist -f %s init' % (loc)) == 0
+    except AssertionError as e:
+        L.error('opersist command failed: %s' % e)
+        exit(1)
