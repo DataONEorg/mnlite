4edc3218eb1f90dbd704fc565677e4b35eb9ba7f
Ian Nesbitt <ian.nesbitt@gmail.com>
2022-12-15 10:27:53 -0800

    adding structure

--- mnonboard/cli.py
+++ mnonboard/cli.py
@@ -1,19 +1,14 @@
 import os, sys
-import subprocess
 import getopt
 
 import utils
 import info_chx
 from defs import CFG, HELP_TEXT
 from mnonboard import L
+from mnonboard import node_path
 from opersist.cli import getOpersistInstance
 
 
-def init_repo(loc):
-    '''
-    Initialize a new instance using opersist.
-    '''
-    subprocess.call('opersist -f %s' % (loc))
 
 
 def run(cfg):
@@ -21,13 +16,19 @@ def run(cfg):
     Wrapper around opersist that simplifies the process of onboarding a new
     member node to DataONE.
     """
-    if cfg['load_json'] is False:
+    if cfg['mode'] is 'user':
         # do the full user-driven info gathering process
         fields = info_chx.user_input()
     else:
         # grab the info from a json
         fields = utils.load_json(cfg['json_file'])
         info_chx.input_test(fields)
+    # now we're cooking
+    # get the node path using the end of the path in the 'subject' field
+    loc = node_path(fields['node']['subject'].split('/')[-1])
+    # initialize a repository there
+    utils.init_repo(loc)
+    
 
 
 def main():
@@ -41,6 +42,26 @@ def main():
             )[0]
     except Exception as e:
         L.error('Error: %s' % e)
+        print(HELP_TEXT)
+        exit(1)
+    for o, a in opts:
+        if o in ('-h', '--help'):
+            print(HELP_TEXT)
+            exit(0)
+        if o in ('-i', '--init'):
+            # do data gathering
+            CFG['mode'] = 'user'
+            run(CFG)
+        if o in ('-d', '--dump='):
+            # dump default json to file
+            utils.save_json(a, utils.default_json())
+        if o in ('-l', '--load='):
+            # load from json file
+            CFG['mode'] = 'json'
+            CFG['json_file'] = a
+            run(CFG)
+
+
 
 if __name__ == '__main__':
     main() # type: ignore
