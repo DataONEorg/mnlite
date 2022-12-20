adfec7484a7a12dc1e855f0b788975ef4b23f9fb
Ian Nesbitt <ian.nesbitt@gmail.com>
2022-12-19 18:43:40 -0800

    adding structure to primary function

--- mnonboard/cli.py
+++ mnonboard/cli.py
@@ -5,7 +5,6 @@ import utils
 import info_chx
 from defs import CFG, HELP_TEXT
 from mnonboard import L
-from mnonboard import node_path
 
 def run(cfg):
     """
@@ -13,25 +12,43 @@ def run(cfg):
     member node to DataONE.
     """
     fields = utils.default_json()
+    names = {}
     if cfg['mode'] == 'user':
         # do the full user-driven info gathering process
-        ufields, dbinfo = info_chx.user_input()
+        ufields, names = info_chx.user_input()
         fields = info_chx.transfer_info(ufields)
     else:
         # grab the info from a json
         fields = utils.load_json(cfg['json_file'])
         info_chx.input_test(fields)
+        # still need to ask the user for some names
+        for f in ('default_owner', 'default_submitter'):
+            names[f] = info_chx.orcid_name(fields[f], f)
+        f = 'contact_subject'
+        names[f] = info_chx.orcid_name(fields['node'][f], f)
     # now we're cooking
-    # get the node path using the end of the path in the 'subject' field
-    loc = node_path(nodedir=fields['node']['subject'].split('/')[-1])
-    # initialize a repository there
+    # get the node path using the end of the path in the 'subject' field (differs from operation.md documentation)
+    end_node_subj = fields['node']['subject'].split('/')[-1]
+    loc = utils.node_path(nodedir=end_node_subj)
     utils.dumps_json(fields)
+    print(names)
+    # initialize a repository there (step 5)
     utils.init_repo(loc)
     for f in ('default_owner', 'default_submitter'):
-        name = info_chx.orcid_name(fields[f], f)
-        utils.new_subject(loc, name, fields[f])
-    name = info_chx.orcid_name(fields['node']['contact_subject'], ['contact_subject'])
-    utils.new_subject(loc, name, fields['node']['contact_subject'])
+        # add a subject for owner and submitter (may not be necessary)
+        utils.new_subject(loc=loc, name=names[f], value=fields[f])
+    f = 'contact_subject'
+    # add subject for technical contact (step 6)
+    utils.new_subject(loc=loc, name=names[f], value=fields['node'][f])
+    # add node as a subject (step 7)
+    utils.new_subject(loc=loc, name=end_node_subj, value=fields['node']['node_id'])
+    # okay, now overwrite the default node.json with our new one (step 8)
+    utils.save_json(loc=os.path.join(loc, 'node.json'), jf=fields)
+    # restart the mnlite process to pick up the new node.json (step 9)
+    utils.restart_mnlite()
+    # run scrapy to harvest metadata (step 10)
+    utils.harvest_data(loc, end_node_subj)
+    # now run tests
 
 
 def main():
