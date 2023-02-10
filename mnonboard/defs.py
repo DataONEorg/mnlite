15ff27219f306913154bfd57552a775b4ba271b6
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-02-10 09:28:46 -0800

    changing severity of keyword violation

--- mnonboard/defs.py
+++ mnonboard/defs.py
@@ -118,7 +118,6 @@ SHACL_ERRORS = {
     # schema.org violations
     'SO:Dataset-description': 'Dataset must have a description',
     'SO:Dataset-identifier': 'Dataset identifiers must be a URL, Text or PropertyValue',
-    'SO:Dataset-keywords': 'A Dataset should include descriptive keywords as literals or DefinedTerm',
     'SO:Dataset-name': 'Name is required for a Dataset',
     'SO:Dataset-url': 'Dataset requires a URL for the location of a page describing the dataset',
     'SO:Dataset-version': 'Dataset must have a version as Literal or Number',
@@ -126,6 +125,7 @@ SHACL_ERRORS = {
   'optional': {
     # schema.org violations
     'SO:Dataset-isAccessibleForFree': 'It is recommended that a Dataset indicates accessibility for free or otherwise',
+    'SO:Dataset-keywords': 'A Dataset should include descriptive keywords as literals or DefinedTerm',
     'SO:Dataset-sameAs': 'It is recommended that a Dataset includes a sameAs URL',
     # SO coordinates
     'schema:GeoCoordinates-longitude': 'It is recommended that a Dataset has a longitude coordinate in WGS-84 format',
