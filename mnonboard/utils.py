24a072a607bea46a6c857a314182fcb33392ff14
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-05-25 21:26:40 +0000

    trying fix for ssh command

--- mnonboard/utils.py
+++ mnonboard/utils.py
@@ -318,9 +318,9 @@ def upload_xml(files, server):
     L.info('Running "ssh %s \'mkdir -p ~/d1_xml/\'"' % (server))
     try:
         op = 'mkdir on remote server'
-        subprocess.run(['ssh', server, "'mkdir -p ~/d1_xml/'"], check=True)
+        subprocess.run(['ssh', server, "mkdir -p ~/d1_xml/"], check=True)
         for fn in files:
             op = 'scp to remote server'
-            subprocess.run(['cat', fn, '|', 'ssh', server, "'cat >> ~/d1_xml/%s'" % fn], check=True)
+            subprocess.run(['cat', fn, '|', 'ssh', server, "cat >> ~/d1_xml/%s" % fn], check=True)
     except Exception as e:
         L.error('%s running %s. Details: %s' % (repr(e), op, e))
