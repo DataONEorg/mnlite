ae560498f0738579a4b5901d34037ca1121a1840
Ian Nesbitt <ian.nesbitt@gmail.com>
2023-10-06 21:21:50 +0000

    starting #32

--- mnonboard/utils.py
+++ mnonboard/utils.py
@@ -11,15 +11,13 @@ from mnonboard.defs import SCHEDULES, NAMES_DICT
 from mnonboard import NODE_PATH_REL, CUR_PATH_ABS, LOG_DIR, HARVEST_LOG_NAME, HM_DATE, L
 from mnonboard.info_chx import cn_subj_lookup, local_subj_lookup, enter_schedule, orcid_name, set_role
 
-def load_json(loc):
+def load_json(loc: str):
     """
     Load json from file.
 
-    Args:
-        loc (str): File location of the json file to be loaded.
-
-    Returns:
-        j (str): Serialized json file contents.
+    :param str loc: File location of the json file to be loaded
+    :returns: Serialized json file contents
+    :rtype: dict
     """
     L.info('Loading member node json from %s' % loc)
     try:
@@ -34,16 +32,12 @@ def load_json(loc):
         L.error('Error: %s' % e)
         exit(1)
 
-def save_json(loc, jf):
+def save_json(loc: str, jf: dict):
     """
     Output json to file.
 
-    Args:
-        loc (str): File location where the json file is to be written.
-        jf (dict): Dictionary to be written as json.
-
-    Returns:
-        (No variable is returned)
+    :param str loc: File location where the json file is to be written.
+    :param dict jf: Dictionary to be written as json.
     """
     L.info('Writing member node json to %s' % loc)
     try:
@@ -58,14 +52,13 @@ def save_json(loc, jf):
         L.error('Error: %s' % e)
         exit(1)
 
-def save_report(rep_str, loc, format='.csv'):
+def save_report(rep_str: str, loc: str, format: str='.csv'):
     """
     Output a validation report for a set of metadata.
 
-    Args:
-        rep_str (str): Report string to be written.
-        loc (str): File location where the report file is to be written.
-        jf (dict): File extension (default: .csv).
+    :param str rep_str: Report string to be written.
+    :param str loc: File location where the report file is to be written.
+    :param str jf: File extension (default: .csv).
     """
     fn = os.path.join(loc, 'report-%s%s' % (HM_DATE, format))
     L.info('Writing report to %s' % (fn))
@@ -77,33 +70,29 @@ def dumps_json(js):
     """
     Quick and dirty way to output formatted json.
 
-    Args:
-        js (dict): Dictionary to be written as json.
+    :param dict js: Dictionary to be written as json.
     """
     print(json.dumps(js, indent=2))
 
-def node_path(nodepath=NODE_PATH_REL, curpath=CUR_PATH_ABS, nodedir=''):
+def node_path(nodepath: str=NODE_PATH_REL, curpath: str=CUR_PATH_ABS, nodedir: str=''):
     """
     Get the absolute path of the nodes directory where new members will go.
     Currently the nodes directory lives at `../instance/nodes/` (relative to
     the mnonboard dir that this file is in).
 
-    Args:
-        nodepath (str): Location of the nodes directory relative to the project directory (default: 'instance/nodes/').
-        curpath (str): Current absolute path of this function (default: os.path.dirname(os.path.abspath(__file__))).
-        nodedir (str): Name of the node directory (example: 'HAKAI_IYS'; default: '')
-
-    Returns:
-        (str): Absolute path of the node directory
+    :param str nodepath: Location of the nodes directory relative to the project directory (default: 'instance/nodes/').
+    :param str curpath: Current absolute path of this function (default: os.path.dirname(os.path.abspath(__file__))).
+    :param str nodedir: Name of the node directory (example: 'HAKAI_IYS'; default: '')
+    :returns: Absolute path of the node directory
+    :rtype: str
     """
     return os.path.abspath(os.path.join(curpath, '../', nodepath, nodedir))
 
-def init_repo(loc):
+def init_repo(loc: str):
     '''
     Initialize a new instance using opersist.
 
-    Args:
-        loc (str): Location of the member node directory in which to initialize an opersist instance.
+    :param str loc: Location of the member node directory in which to initialize an opersist instance.
     '''
     try:
         L.info('Using opersist to init new member node folder: %s' % loc)
@@ -114,14 +103,13 @@ def init_repo(loc):
         L.error('opersist init command failed (node folder: %s): %s' % (loc, e))
         exit(1)
 
-def new_subj(loc, name, value):
+def new_subj(loc: str, name: str, value: str):
     """
     Create new subject in the database using opersist.
 
-    Args:
-        loc (str): Location of the opersist instance.
-        name (str): Subject name (human readable).
-        value (str): Subject value (unique subject id, such as orcid or member node id).
+    :param str loc: Location of the opersist instance.
+    :param str name: Subject name (human readable).
+    :param str value: Subject value (unique subject id, such as orcid or member node id).
     """
     try:
         L.info('opersist creating new subject. Name: %s Value: %s Location: %s' % (name, value, loc))
@@ -135,18 +123,18 @@ def new_subj(loc, name, value):
         L.error('opersist subject creation command failed for %s (%s): %s' % (name, value, e))
         exit(1)
 
-def get_or_create_subj(loc, value, cn_url, title='unspecified subject', name=None):
+def get_or_create_subj(loc: str, value: str, cn_url: str, title: str='unspecified subject', name: str=None):
     """
     Get an existing subject using their ORCiD or create a new one with the specified values.
     Search is conducted first at the given coordinating node URL, then locally.
     If no subject is found, a new record is created in the local opersist instance.
 
-    Args:
-        loc (str): Location of the opersist instance.
-        value (str): Subject value (unique subject id, such as orcid or member node id).
-        cn_url (str): The base URL of the rest API with which to search for the given subject.
-        title (str): The subject's role in relation to the database.
-        name (str or bool): Subject name (human readable).
+    :param str loc: Location of the opersist instance.
+    :param str value: Subject value (unique subject id, such as orcid or member node id).
+    :param str cn_url: The base URL of the rest API with which to search for the given subject.
+    :param str title: The subject's role in relation to the database.
+    :param name: Subject name (human readable).
+    :type name: str or None
     """
     if name:
         # we are probably creating a node record
@@ -171,8 +159,8 @@ def set_schedule():
     Ask the user what schedule on which they would like to run scrapes.
     Options are: monthly, daily, and every 3 minutes.
 
-    Returns:
-        (dict): Dictionary entry formatted based on the chosen schedule option.
+    :returns: Dictionary entry formatted based on the chosen schedule option.
+    :rtype: dict
     """
     s = enter_schedule()
     return SCHEDULES[s]
@@ -209,11 +197,12 @@ def restart_mnlite():
             print('You have selected an invalid option.')
 
 
-def harvest_data(loc, mn_name):
+def harvest_data(loc: str, mn_name: str):
     """
-    Args:
-        loc (str): Location of the opersist instance.
-        mn_name (str): Name of the member node (used to name the crawl log).
+    Use scrapy to harvest data to the specified path, and output a log to the specified location.
+
+    :param str loc: Location of the opersist instance.
+    :param str mn_name: Name of the member node (used to name the crawl log).
     """
     log_loc = os.path.join(LOG_DIR, mn_name + HARVEST_LOG_NAME)
     L.info('Starting scrapy crawl, saving to %s' % (loc))
@@ -227,18 +216,16 @@ def harvest_data(loc, mn_name):
     except Exception as e:
         L.error('Error running scrapy: %s' % e)
 
-def limit_tests(num_things):
+def limit_tests(num_things: int):
     """
     Ask the user to limit the number of tests to run on a given set of metadata.
     This will execute if the user decides to try and test more than 500 metadata objects.
     The prompt will ask them if they wish to limit the number, then return a
     number based on their decision.
 
-    Args:
-        num_things (int): Initial number of things to test.
-
-    Returns:
-        num_things (int): Modified number of things to test.
+    :param int num_things: Initial number of things to test.
+    :returns: Modified number of things to test.
+    :rtype: int
     """
     while True:
         i = input('Testing more than 500 objects is not recommended due to performance concerns.\n\
@@ -281,17 +268,16 @@ def ask_continue(msg: str):
             print('You have entered an incorrect value. Please enter "y" to continue or "n" to quit.')
             continue
 
-def create_names_xml(loc, node_id, names):
+def create_names_xml(loc: str, node_id: str, names: dict):
     """
     Format subject XML documents and return list of names.
 
-    Args:
-        loc (str): Location (dir) to write file to.
-        node_id (str): Node id of current MN.
-        names (dict): Dict of subject names with ORCiD as index.
+    :param str loc: Location (dir) to write file to.
+    :param str node_id: Node id of current MN.
+    :param dict names: Dict of subject names with ORCiD as index.
 
-    Returns:
-        files (list): List of files written.
+    :returns: List of files written.
+    :rtype: list
     """
     # make dir
     loc = os.path.join(loc, 'xml')
