f1981d62cf738805f7683c6dfcdf1327e6a1e753
Ian Nesbitt <ian.nesbitt@gmail.com>
2024-08-30 22:16:48 +0000

    adding some CN operations

--- mnonboard/cn.py
+++ mnonboard/cn.py
@@ -1,6 +1,7 @@
 import logging
 from d1_client.cnclient import CoordinatingNodeClient
 from d1_common.types.dataoneTypes import Subject, person
+from opersist import OPersist
 #import d1_admin_tools as d1np
 
 from . import defs, utils
@@ -89,3 +90,94 @@ def set_nodes_properties(nodes_properties: dict, con=None):
     :param dict nodes_properties: 
     :param con: 
     """
+
+def set_obsoleted_by(client: CoordinatingNodeClient, pid: str, obsoleted_by: str):
+    """
+    Set the obsoletedBy property of a PID on the CN.
+
+    :param d1_client.cnclient.CoordinatingNodeClient client: The client to use for the query
+    :param str pid: The PID to set the obsoletedBy property for
+    :param str obsoleted_by: The PID to set as the obsoletedBy property
+    """
+    L = logging.getLogger(__name__)
+    try:
+        return client.setObsoletedBy(pid=pid, obsoletedByPid=obsoleted_by)
+    except Exception as e:
+        L.error(repr(e))
+
+def get_objects_by_node(client: CoordinatingNodeClient, node_id: str):
+    """
+    Get a list of objects by node from the CN.
+
+    :param d1_client.cnclient.CoordinatingNodeClient client: The client to use for the query
+    :param str node_id: The node ID to get the objects for
+    :returns: A list of objects
+    :rtype: list
+    """
+    L = logging.getLogger(__name__)
+    objects = []
+    while True:
+        try:
+            l = client.listObjects(nodeId=node_id, start=len(objects), count=1000).content()
+            objects.append(l)
+            if len(l) < 1000:
+                break
+        except Exception as e:
+            L.error(e)
+            break
+    if len(objects) > 0:
+        return objects
+
+def get_last_object_in_series(objects: list, series_id: str):
+    """
+    Get the last object in a series from the CN.
+
+    Assumes the version chain is intact.
+
+    :param list objects: The list of objects to search
+    :param str series_id: The series ID
+    """
+    if objects:
+        for obj in objects:
+            if obj.seriesId == series_id:
+                if obj.obsoletedBy:
+                    continue
+                else:
+                    return obj.identifier
+
+def chain_check(objects: list, sid: str, op: OPersist):
+    """
+    Check the version chain of a SID on the CN.
+    If
+
+    :param list objects: The list of objects to search
+    :param str pid: The PID to check the version chain for
+    """
+    L = logging.getLogger(__name__)
+    chain = []
+    for obj in objects:
+        if obj.seriesId == sid:
+            chain.append(obj)
+    brk = False
+    L.info(f'{sid} checking {len(chain)} objects in version chain')
+    if len(chain) > 0:
+        pids = [x.identifier for x in chain]
+        for i in range(len(chain)):
+            if chain[i].obsoletedBy:
+                if chain[i].obsoletedBy in pids:
+                    continue
+                else:
+                    L.info('CN version chain broken at %s' % chain[i].identifier)
+                    # check if the obsoletedBy pid is in the OPersist instance
+                    thing = op.getThing(chain[i].obsoletedBy)
+                    if thing:
+                        L.info(f'CN object {chain[i].obsoletedBy} is obsoletedBy OPersist object {thing}')
+                    else:
+                        L.error(f'OPersist object {chain[i].obsoletedBy} not found!')
+            else:
+                L.info(f'{sid} version chain broken at {chain[i].identifier}')
+                if brk:
+                    L.error(f'{sid} version chain broken again at {chain[i].identifier}')
+                else:
+                    brk = True
+
